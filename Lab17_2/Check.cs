b35f07e226dfaff636d671d6f1992c4467316c16
AntoninaVish <vishivtonya@gmail.com>
2022-01-05 15:29:21 +0300

    Добавьте файлы проекта.

---
+++ Lab17_2/Check.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17_2
+{
+    class Check <T>
+    {
+        private T num;
+        private double balance;
+        private string name;
+        
+        public T Num // свойство
+        {
+            set
+            {
+                num = value;
+            }
+            get
+            {
+                return num;
+            }
+        }
+        public double Balance // свойство
+        {
+            set
+            {
+               if (value>=0)
+               {
+                    balance = value;
+               }
+                else
+                {
+                    Console.WriteLine("Баланс не может быть отрицательным");
+                }
+            }
+            get
+            {
+                return balance;
+            }
+        }
+        public string Name // свойство 
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        public Check(T num, double balance, string name)//конструктор
+        {
+            Num = num; // параметры конструктора
+            Balance = balance;
+            Name = name;
+        }
+        public void GetInfo() // метод для чтения
+        {
+            Console.WriteLine ($"Номер счета: {Num}; баланс: {Balance}; Ф.И.О: {Name}");
+        }
+        
+                 
+    }
+
+}
---
+++ Lab17_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числовой номер:");
+            int numInt = Convert.ToInt32(Console.ReadLine()); //метод для заполнения пользователем
+            Console.WriteLine("Введите баланс:");
+            double balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите Ф.И.О:");
+            string name = Console.ReadLine();
+            Check<int> check1 = new Check<int> (numInt,balance,name); // создаем экземпляр класса Check
+            check1.GetInfo(); // вызываем метод
+
+            Console.WriteLine("Введите строковый номер:");
+            string numString = Console.ReadLine();
+            Console.WriteLine("Введите баланс:");
+            balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите Ф.И.О:");
+            name = Console.ReadLine();
+            Check<string> check2 = new Check<string>(numString, balance, name);
+            check2.GetInfo();
+
+            Console.ReadKey();
+
+        }
+    }
+}
